173818c32a018c70732d89e28b6946bbcd1d76b5
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-01-04 13:23:40 -0300

    project created

---
+++ backend/src/main/java/com/br/estudos/dsdeliever/DsdelieverApplication.java
@@ -0,0 +1,13 @@
+package com.br.estudos.dsdeliever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdelieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdelieverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/br/estudos/dsdeliever/DsdelieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.estudos.dsdeliever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdelieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
